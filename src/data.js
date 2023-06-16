b4b2b0858706d14caea44d870db70b4ed73853f6
Coding4Toon <coding4toon@gmail.com>
2023-06-16 05:53:43 -0500

    First commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Video File',
+        type: 'video',
+        path: "/file-server/video.mp4",
+        size: 10810
+    },
+    {
+        id: 2,
+        name: 'Audio File',
+        type: 'audio',
+        path: "/file-server/audio.mp3",
+        size: 6178
+    },
+    {
+        id: 3,
+        name: 'Document File',
+        type: 'document',
+        path: "/file-server/document.pdf",
+        size: 701
+    },
+    {
+        id: 4,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/image.jpg",
+        size: 677
+    },
+    {
+        id: 5,
+        name: 'Image File 2',
+        type: 'image',
+        path: "/file-server/image2.jpg",
+        size: 3745
+    },
+    {
+        id: 6,
+        name: 'Image File 3',
+        type: 'image',
+        path: "/file-server/image3.jpg",
+        size: 1878
+    },
+    {
+        id: 7,
+        name: 'Document File 2',
+        type: 'document',
+        path: "/file-server/document2.pdf",
+        size: 10747
+    },
+    {
+        id: 8,
+        name: 'Document File 3',
+        type: 'document',
+        path: "/file-server/document3.pdf",
+        size: 272
+    },
+]
